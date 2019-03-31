fe94f951688ad79115486a5cdfb6ced69dd49b56
Arve Seljebu <arve.seljebu@gmail.com>
2019-03-31 21:05:31 +0200

    proper executable check

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ curl --silent https://api.github.com/repos/arve0/foreground-cron/releases/latest
   | cut -d '"' -f 4 \
   | wget -qP ~/.bin/ -i - &&
 chmod +x ~/.bin/foreground-cron &&
-type -P foreground-cron > /dev/null || {
+if ! [ -x "$(command -v foreground-cron)" ]; then
   echo 'export PATH="$HOME/.bin:$PATH"' >> ~/.bash_profile;
   echo "Added 'export PATH=\"\$HOME/.bin:\$PATH\"' to ~/.bash_profile";
   echo "Run 'source ~/.bash_profile' to enable new path or restart your shell"
-}
+fi
